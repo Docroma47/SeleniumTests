76475e1f67cfcc5227738e2344f18ba94b539b71
Roman <mom47@yandex.ru>
2020-03-18 19:49:04 +0300

    Learning "findElement" commands.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,29 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "A:\\Proj\\TestSelenium\\drivers\\geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
-        driver.manage().window().setSize(new Dimension(900, 500));
+        driver.get("http://en.wikipedia.org");
+        WebElement link = driver.findElement(By.linkText("Log in"));
+        WebElement link2 = driver.findElement(By.partialLinkText("Donate"));
+        WebElement serchField = driver.findElement(By.name("search"));
+        WebElement searchButton = driver.findElement(By.className("searchButton"));
+        WebElement searchId = driver.findElement(By.id("ca-viewsource"));
+        WebElement searchInput = driver.findElement(By.tagName("input"));
+        WebElement searcCSS = driver.findElement(By.cssSelector("div#p-search"));
+        WebElement logo = driver.findElement(By.xpath("//div[@id='p-logo']/a"));
 
-        driver.get("https://github.com");
-        driver.navigate().to("http://seleniumhq.org");
-        driver.navigate().back();
-        driver.navigate().forward();
-        driver.navigate().refresh();
-
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
         driver.quit();
     }
 

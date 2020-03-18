be011664d6724c5ee8fa5d9fbde7e46bdf726245
Roman <mom47@yandex.ru>
2020-03-18 19:10:31 +0300

    Learning navigation commands.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -7,7 +8,17 @@ public class Main {
 
         WebDriver driver = new FirefoxDriver();
 
+        driver.manage().window().setSize(new Dimension(900, 500));
+
         driver.get("https://github.com");
+        driver.navigate().to("http://seleniumhq.org");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
     }
 
 }

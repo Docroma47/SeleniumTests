c6db062312128f2e54c2329becfe15a979745c81
Roman <mom47@yandex.ru>
2020-03-18 22:17:14 +0300

    Work with links.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,16 +15,23 @@ public class Main {
         driver.manage().window().maximize();
 
         driver.get("http://en.wikipedia.org");
-        driver.findElement(By.xpath("//*[@id='searchInput']")).sendKeys(new String[]{"Selenium Webdriver"});
-        driver.findElement(By.xpath("//*[@id='searchButton']")).click();
-        System.out.println(driver.findElement(By.xpath("//*[@id='ooui-php-1']")).getAttribute("Value"));
-        driver.findElement(By.xpath("//*[@id='ooui-php-1']")).clear();
 
+        WebElement link = driver.findElement(By.xpath("//li[@id=\"n-aboutsite\"]/a"));
+        System.out.println(link.getText());
+        link.click();
+
+//        driver.findElement(By.xpath("//*[@id='searchInput']")).sendKeys(new String[]{"Selenium Webdriver"});
+//        driver.findElement(By.xpath("//*[@id='searchButton']")).click();
+//        System.out.println(driver.findElement(By.xpath("//*[@id='ooui-php-1']")).getAttribute("Value"));
+//        driver.findElement(By.xpath("//*[@id='ooui-php-1']")).clear();
+//
         driver.get("https://github.com");
-        driver.findElement(By.xpath("//*[@id=\"user[login]\"]")).sendKeys(new String[]{"testusername"});
-        driver.findElement(By.xpath("//*[@id=\"user[password]\"]")).sendKeys(new String[]{"testpass"});
-        WebElement button = driver.findElement(By.xpath("/html/body/div[4]/main/div[1]/div/div/div[2]/div[1]/form/button"));
-        button.submit();
+        driver.findElement(By.xpath("//li[@class='border-bottom border-lg-bottom-0 mr-0 mr-lg-3']/a")).click();
+
+//        driver.findElement(By.xpath("//*[@id=\"user[login]\"]")).sendKeys(new String[]{"testusername"});
+//        driver.findElement(By.xpath("//*[@id=\"user[password]\"]")).sendKeys(new String[]{"testpass"});
+//        WebElement button = driver.findElement(By.xpath("/html/body/div[4]/main/div[1]/div/div/div[2]/div[1]/form/button"));
+//        button.submit();
 
 //        WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
 //        if (button.getText().equals("Sign up for GitHub")) System.out.println("Success!");
@@ -34,12 +41,13 @@ public class Main {
 //
 //        driver.findElement(By.xpath("/html/body/div[1]/header/div/div[2]/div[2]/a[1]")).click();
 //
-        driver.get("https://www.facebook.com/");
-        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys(new String[]{"testName"});
-        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys(new String[]{"testPassword"});
-        WebElement logInButton = driver.findElement(By.xpath("//*[@id=\"u_0_b\"]"));
-        logInButton.submit();
-        System.out.println("Your eMail is: " + driver.findElement(By.xpath("//*[@autocomplete='username']")).getAttribute("Value"));
+        driver.get("https://www.facebook.com");
+        driver.findElement(By.xpath("//td[@class=\"login_form_label_field\"]/div/a")).click();
+//        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys(new String[]{"testName"});
+//        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys(new String[]{"testPassword"});
+//        WebElement logInButton = driver.findElement(By.xpath("//*[@id=\"u_0_b\"]"));
+//        logInButton.submit();
+//        System.out.println("Your eMail is: " + driver.findElement(By.xpath("//*[@autocomplete='username']")).getAttribute("Value"));
     }
 
 }

956ca018c3e6740e931f586ef18375f7bf569f9d
Roman <mom47@yandex.ru>
2020-03-19 22:08:36 +0300

    Check for item availability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,51 +11,29 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     static WebDriver driver;
-    static WebDriver driverChrome;
-//    static WebDriverWait wait;
-//    static WebDriverWait wait2;
+//    static WebDriver driverChrome;
+    static WebDriverWait wait;
+    static WebDriverWait wait2;
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.gecko.driver", "A:\\Proj\\TestSelenium\\drivers\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "A:\\Proj\\TestSelenium\\drivers\\chromedriver.exe");
 
-//        driver = new FirefoxDriver();
-        driverChrome = new ChromeDriver();
-//        wait  = (new WebDriverWait(driver, 5));
-//        wait2  = (new WebDriverWait(driver, 10));
+        driver = new FirefoxDriver();
+//        driverChrome = new ChromeDriver();
+        wait  = (new WebDriverWait(driver, 5));
+        wait2  = (new WebDriverWait(driver, 10));
 
-        driverChrome.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driverChrome.manage().window().maximize();
-//        driverChrome.get("https://basicweb.ru/html/primer/41a.php");
-//        WebElement buttom1 = driverChrome.findElement(By.xpath("//button[text()='Элемент отключен']"));
-//        WebElement buttom2 = driverChrome.findElement(By.xpath("//button[text()='Элемент активен']"));
-//
-//        System.out.println(buttom1.isEnabled());
-//        System.out.println(buttom2.isEnabled());
-//
-//        if (buttom1.isEnabled()) driverChrome.get("https://mail.ru");
-//        if (buttom2.isEnabled()) driverChrome.get("https://account.mail.ru/signup?from=main&rf=auth.mail.ru");
-//        WebElement radioBut = driverChrome.findElement(By.xpath("//label[@data-test-id=\"gender-male\"]//input"));
-//        WebElement radioBut2 = driverChrome.findElement(By.xpath("//label[@data-test-id=\"gender-female\"]//input"));
-//
-//        if (!(radioBut.isSelected())) {
-//            driverChrome.findElement(By.xpath("//label[@data-test-id=\"gender-male\"]//span")).click();
-//        }
-//
-//        if (radioBut2.isSelected()) {
-//            System.out.println("WAAAT?!");
-//        }
-        driverChrome.get("http://ebay.com");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://twitter.com/");
 
-        WebElement link1 = driverChrome.findElement(By.xpath("//a[text()='Samsung']"));
-        WebElement link2 = driverChrome.findElement(By.xpath("//li[@class=\"hl-cat-nav__js-tab\"]//a[text()='Мода']"));
 
-        System.out.println(link1.isDisplayed());
-        System.out.println(link2.isDisplayed());
-
-        if (link1.isDisplayed()) link1.click();
-        else link2.click();
-
+        if (driver.findElements(By.xpath("//span[text()='Registration']")).size() > 0) {
+            System.out.println("Such element is presented");
+        } else {
+            System.out.println("Cannot find such element");
+        }
     }
 
 }
